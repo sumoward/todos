f11bc7c2fcd59dd747bad5c61790cbd7844468ba
brian.w@edgescan.com <Pa55word123!>
2016-06-23 10:19:05 +0100

    add email

---
+++ spec/features/user_sees_own_todos_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature 'User sees own todos' do
+  scenario 'doesnt see others todos' do
+    Todo.create!(title: "Buy Milk", email: "someone_else@example.com")
+    sign_in_as "someone@example.com"
+    expect(page).not_to have_css ".todos li", text: "Buy Milk"
+  end
+end
